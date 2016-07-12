509d6e086f2b25c65a2d3cff75433d7c051f4234
HFCherish <hf_cherish@163.com>
2016-07-12 21:40:04 +0800

    added test for orderRepository

--- src/test/java/com/thoughtworks/api/repository/OrderRepositoryTest.java
+++ src/test/java/com/thoughtworks/api/repository/OrderRepositoryTest.java
@@ -1,18 +1,53 @@
 package com.thoughtworks.api.repository;
 
+import com.thoughtworks.api.records.Order;
+import com.thoughtworks.api.records.OrderItem;
+import com.thoughtworks.api.records.Product;
+import com.thoughtworks.api.records.User;
 import com.thoughtworks.api.support.ApiSupport;
 import com.thoughtworks.api.support.ApiTestRunner;
+import com.thoughtworks.api.support.TestHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.inject.Inject;
+
+import java.util.Optional;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 @RunWith(ApiTestRunner.class)
 public class OrderRepositoryTest extends ApiSupport{
+    @Inject
+    UserRepository userRepository;
+
+    @Inject
+    ProductRepository productRepository;
+
+    @Inject
+    OrderRepository orderRepository;
+
+    private User user;
+    private Product product;
 
     @Test
-    public void should_create_order() {
+    public void should_create_and_get_order() {
+        user = TestHelper.prepareUser(userRepository);
+        product = TestHelper.prepareProduct(productRepository);
+        Order order = TestHelper.orderForTest(product);
 
+        orderRepository.save(order, user.getId());
+        final Optional<Order> fetched = orderRepository.ofId(order.getId());
 
+        assertThat(fetched.isPresent(), is(true));
+        final Order fetchedOrder = fetched.get();
+        assertThat(fetchedOrder.getUserId(), is(user.getId()));
+        assertThat(fetchedOrder.getId(), is(order.getId()));
+        assertThat(fetchedOrder.getTotalPrice(), is(order.getTotalPrice()));
+        assertThat(fetchedOrder.getOrderItems().size(), is(1));
+        final OrderItem orderItem = fetchedOrder.getOrderItems().get(0);
+        assertThat(orderItem.getProductId(), is(product.getId()));
+        assertThat(orderItem.getAmount(), is(2.2));
     }
 }
